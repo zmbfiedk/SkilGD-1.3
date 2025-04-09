593a91358259957bd8242c0c46a1fe72ce8c7c22
Arthur <lotuslovesurmom@gmail.com>
2025-04-09 14:12:54 +0200

    Byte update fix

--- Assets/Scripts/Byte.cs
+++ Assets/Scripts/Byte.cs
@@ -5,6 +5,48 @@ public class Byte : MonoBehaviour
     [SerializeField] Bit[] bits = new Bit[8];
     [SerializeField] private int value = 0;
 
+    private void Update()
+    {
+        bin2dec();
+    }
+
+    private void bin2dec()
+    {
+        value = 0;
+        if (bits[0].state)
+        {
+            value += 1;
+        }
+        if (bits[1].state)
+        {
+            value += 2;
+        }
+        if (bits[2].state)
+        {
+            value += 4;
+        }
+        if (bits[3].state) 
+        {
+            value += 8;
+        }
+        if (bits[4].state)
+        {
+            value += 16;
+        }
+        if (bits[5].state)
+        {
+            value += 32;
+        }
+        if (bits[6].state)
+        {
+            value += 64;
+        }
+        if (bits[7].state)
+        {
+            value += 128;
+        }
+    }
+
     public void SetValue(int newValue)
     {
         if (newValue > 255)
